f046c6db494cab07690cda2c7ffdf27490675bcb
dataSeb-ops <bmbry19@gmail.com>
2025-09-13 18:11:59 -0500

    Separated queries by city
    Separated queries examining data by cities to look at total number vs percentage

---
+++ techCoResearch/queries/02_layoffs_by_city_num.sql
@@ -0,0 +1,6 @@
+-- NUMBER OF TECH LAYOFFS BY CITY
+SELECT	headquarter_city, SUM(total_layoffs) AS city_total_layoffs, SUM(impacted_workforce_percentage) AS city_percent_layoffs
+FROM	cleaned_table
+GROUP	BY headquarter_city
+ORDER	BY city_total_layoffs DESC
+LIMIT	5;
--- techCoResearch/queries/03_layoffs_by_city_percent.sql
+++ techCoResearch/queries/03_layoffs_by_city_percent.sql
@@ -1,10 +1,3 @@
--- NUMBER OF TECH LAYOFFS BY CITY
-SELECT	headquarter_city, SUM(total_layoffs) AS city_total_layoffs, SUM(impacted_workforce_percentage) AS city_percent_layoffs
-FROM	cleaned_table
-GROUP	BY headquarter_city
-ORDER	BY city_total_layoffs DESC
-LIMIT	5;
-
 -- PERCENT OF TECH LAYOFFS BY CITY
 SELECT	headquarter_city, SUM(total_layoffs) AS city_total_layoffs, SUM(impacted_workforce_percentage) AS city_percent_layoffs
 FROM	cleaned_table
