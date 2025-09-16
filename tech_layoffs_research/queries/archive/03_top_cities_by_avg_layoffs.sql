0d45aba2b3428184ac9538a5ac17aa39db971bc9
dataSeb-ops <bmbry19@gmail.com>
2025-09-16 11:33:29 -0500

    Removed DS_Store
    Removed archive and DS_Store files instead of just ignoring them

--- tech_layoffs_research/queries/archive/03_top_cities_by_avg_layoffs.sql
+++
@@ -1,7 +0,0 @@
--- AVG NUMBER OF LAYOFFS PER COMPANY BY CITY
-SELECT	headquarter_city,
-		SUM(total_layoffs)/COUNT(total_layoffs) AS city_avg_layoffs
-FROM	cleaned_table
-GROUP	BY headquarter_city
-ORDER	BY city_avg_layoffs DESC
-LIMIT	5;
