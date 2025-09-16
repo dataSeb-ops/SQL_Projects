965cab871d88aa2c80ed2945c6aa18792d2416f4
dataSeb-ops <bmbry19@gmail.com>
2025-09-16 13:10:50 -0500

    Changed folder name to *_analysis

---
+++ tech_layoffs_analysis/queries/archive/01_clean_headquarters.sql
@@ -0,0 +1,29 @@
+/*Examine headquarter_location to identify any distinctions between identical locations
+due to abbreviations, etc.*/
+SELECT	DISTINCT headquarter_location
+FROM	tech_layoffs
+ORDER	BY headquarter_location;
+
+-- CLEANING
+CREATE	VIEW cleaned_table AS
+SELECT	company, CAST(total_layoffs AS INTEGER) AS total_layoffs,
+		impacted_workforce_percentage, reported_date, industry,
+		CASE
+		-- if there is a comma, extract only city
+        WHEN INSTR(headquarter_location, ',') > 0 THEN TRIM(SUBSTR(headquarter_location, 1, INSTR(headquarter_location, ',') - 1))
+		-- get rid of "greater New York area" etc.
+		WHEN headquarter_location LIKE '% New York%' THEN 'New York' 
+		-- misspelling
+		WHEN headquarter_location LIKE '%Franicsco%' THEN 'San Fancisco'
+		-- if no comma or other issues
+		ELSE headquarter_location
+		END AS headquarter_city,
+		sources, status, additional_notes
+FROM	tech_layoffs
+-- get rid of entries where data is unclear
+WHERE	NOT (total_layoffs = 'Unclear' OR impacted_workforce_percentage = 'Unclear');
+
+SELECT	*
+FROM	cleaned_table
+ORDER	BY headquarter_city DESC;
+
---
+++ tech_layoffs_analysis/queries/archive/04_top_cities_by_avg_percent_impacted.sql
@@ -0,0 +1,7 @@
+-- AVG PERCENT OF LAYOFFS BY CITY
+SELECT	headquarter_city,
+		ROUND(AVG(impacted_workforce_percentage),2) AS avg_percent_impacted
+FROM	cleaned_table
+GROUP	BY headquarter_city
+ORDER	BY avg_percent_impacted DESC
+LIMIT	5;
